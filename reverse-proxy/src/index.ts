60f1c760953b85be4084d455ae2aa802d6dc6d73
Van Tri <vtri2016@gmail.com>
2023-05-27 20:20:37 +0700

    done 27/05
    Signed-off-by: Van Tri <vtri2016@gmail.com>

---
+++ reverse-proxy/src/index.ts
@@ -0,0 +1,54 @@
+import http from 'http';
+import httpProxy from 'http-proxy';
+import cors from 'cors';
+
+const proxy = httpProxy.createProxyServer();
+proxy.on('error', function (error, req, res: http.ServerResponse) {
+  console.log('error', error);
+  (res as http.ServerResponse).statusCode = 500;
+  res.end();
+});
+proxy.on('proxyRes', (proxyRes, req, res) => {
+  if (req.headers['access-control-request-method']) {
+    res.setHeader(
+      'access-control-allow-methods',
+      req.headers['access-control-request-method'],
+    );
+  }
+
+  if (req.headers['access-control-request-headers']) {
+    res.setHeader(
+      'access-control-allow-headers',
+      req.headers['access-control-request-headers'],
+    );
+  }
+
+  if (req.headers.origin) {
+    res.setHeader('access-control-allow-origin', req.headers.origin);
+    res.setHeader('access-control-allow-credentials', 'true');
+  }
+});
+
+http
+  .createServer(function (req, res) {
+    const path = req.url;
+
+    if (path.includes('auth-service')) {
+      proxy.web(req, res, {
+        target: `http://localhost:5000`,
+      });
+    }
+
+    if (path.includes('file-service')) {
+      proxy.web(req, res, {
+        target: `http://localhost:5002`,
+      });
+    }
+
+    if (path.includes('post-service')) {
+      proxy.web(req, res, {
+        target: `http://localhost:5001`,
+      });
+    }
+  })
+  .listen(8080, () => console.log('listening at 8080'));
