7ad11bb16e0556b994c8752e4659fb619fa73258
Van Tri <vtri2016@gmail.com>
2023-06-11 23:16:45 +0700

    ''
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- scripts/commit-all.sh
+++ scripts/commit-all.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 
-cd client && git add . && git $1 -m "'"$2"'" && git push & 
-cd auth-service && git add . && git $1 -m "'"$2"'" && git push &
-cd base-service && git add . && git $1 -m "'"$2"'" && git push &
-cd common-service && git add . && git $1 -m "'"$2"'" && git push &
-cd post-service && git add . && git $1 -m "'"$2"'" && git push &
-cd file-service && git add . && git $1 -m "'"$2"'" && git push &
-cd message-service && git add . && git $1 -m "'"$2"'" && git push &
-cd friend-service && git add . && git $1 -m "'"$2"'" && git push &
-cd shared-service && git add . && git $1 -m "'"$2"'" && git push &
-cd file-service && git add . && git $1 -m "'"$2"'" && git push &
+cd client && git add . && git cmm "'"$2"'" && git push & 
+cd auth-service && git add . && git cmm "'"$2"'" && git push &
+cd base-service && git add . && git cmm "'"$2"'" && git push &
+cd common-service && git add . && git cmm "'"$2"'" && git push &
+cd post-service && git add . && git cmm "'"$2"'" && git push &
+cd file-service && git add . && git cmm "'"$2"'" && git push &
+cd message-service && git add . && git cmm "'"$2"'" && git push &
+cd friend-service && git add . && git cmm "'"$2"'" && git push &
+cd shared-service && git add . && git cmm "'"$2"'" && git push &
+cd file-service && git add . && git cmm "'"$2"'" && git push &
 
 wait
 
-git add . && git $1 -m "'"$2"'" && git push
-echo git $1 -m "'"$2"'"
+git add . && git cmm "'"$2"'" && git push
