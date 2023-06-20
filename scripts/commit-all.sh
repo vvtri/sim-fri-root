8ef7144bb181c6955f532a3bc260711484be84ac
Van Tri <vtri2016@gmail.com>
2023-06-20 20:38:22 +0700

    commit 20/06
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- scripts/commit-all.sh
+++ scripts/commit-all.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 
-cd client && git add . && git cmm "'"$1"'" && git push & 
-cd auth-service && git add . && git cmm "'"$1"'" && git push &
-cd base-service && git add . && git cmm "'"$1"'" && git push &
-cd common && git add . && git cmm "'"$1"'" && git push &
-cd post-service && git add . && git cmm "'"$1"'" && git push &
-cd file-service && git add . && git cmm "'"$1"'" && git push &
-cd message-service && git add . && git cmm "'"$1"'" && git push &
-cd friend-service && git add . && git cmm "'"$1"'" && git push &
-cd shared && git add . && git cmm "'"$1"'" && git push &
-cd file-service && git add . && git cmm "'"$1"'" && git push &
-cd noti-service && git add . && git cmm "'"$1"'" && git push &
+cd client && git add . && git cmm $1 && git push & 
+cd auth-service && git add . && git cmm $1 && git push &
+cd base-service && git add . && git cmm $1 && git push &
+cd common && git add . && git cmm $1 && git push &
+cd post-service && git add . && git cmm $1 && git push &
+cd file-service && git add . && git cmm $1 && git push &
+cd message-service && git add . && git cmm $1 && git push &
+cd friend-service && git add . && git cmm $1 && git push &
+cd shared && git add . && git cmm $1 && git push &
+cd file-service && git add . && git cmm $1 && git push &
+cd noti-service && git add . && git cmm $1 && git push &
 
 wait
 
-git add . && git cmm "'"$2"'" && git push
+git add . && git cmm $1 && git push
